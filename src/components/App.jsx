a3f1e4e633e479f9db9b5b7b3075759c35d54808
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-05-18 13:14:10 +0200

    first file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+// import Profile from './Profile/Profile';
+// import user from '../data/user.json';
+// import Statistics from './Statistics/Statistics';
+// import data from '../data/data.json';
+// import FriendList from './FriendList/FriendList';
+// import friends from '../data/friends.json';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
+// import transactions from '../data/transactions.json';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div></div>;
 };
